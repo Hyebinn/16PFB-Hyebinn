781365d5e4b54033bebd5909aeeb0f339316ab77
CAD Client <CAD Client>
2016-05-20 16:39:50 +0900

    added ex32

--- ex32/problem 1.py
+++ ex32/problem 1.py
@@ -1,3 +1,3 @@
 #-*-coding:utf8
 # 1~1000의 수 중 3의 배수이거나 5의 배수인 수에서 1~1000의 수 중 3의 배수이고 5의 배수인 수를 뺀다.
-print(sum((x for x in range(1, 1000) if x % 3 == 0 or x % 5 == 0))-sum(x for x in range(1, 1000)if x %3 ==0 and x % 5==0))
+print(sum((x for x in range(1, 1000) if x % 3 == 0 or x % 5 == 0))-sum(x for x in range(1, 1000)if x %15 ==0))
